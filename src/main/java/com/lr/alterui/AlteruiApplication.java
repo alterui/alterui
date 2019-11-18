e90191a02e1521bc48cfbcc3a1afe911a5500640
liurui <liu.rui@oyohotels.cn>
2019-11-18 21:12:08 +0800

    Initial commit

---
+++ src/main/java/com/lr/alterui/AlteruiApplication.java
@@ -0,0 +1,13 @@
+package com.lr.alterui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlteruiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlteruiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lr/alterui/AlteruiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.alterui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlteruiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
